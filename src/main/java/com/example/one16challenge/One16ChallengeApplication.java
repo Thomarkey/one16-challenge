2093fdf45019928a4852f8d0f7e7d04570a45787
thomarkey <thoma_cz@hotmail.com>
2023-10-17 13:25:03 +0200

    init

---
+++ src/main/java/com/example/one16challenge/One16ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.one16challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class One16ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(One16ChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/one16challenge/One16ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.one16challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class One16ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
