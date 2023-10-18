5b2f85f42edf70885aab331e477c2d14b0d9a595
thomarkey <thoma_cz@hotmail.com>
2023-10-18 12:46:25 +0200

    replaced for loops with streams

--- src/main/java/com/example/one16challenge/model/FileHelper.java
+++ src/main/java/com/example/one16challenge/model/FileHelper.java
@@ -4,23 +4,20 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class FileHelper {
 
     //Create the set of words with specific length
-    public static Set<String> filterWordsByLength(Set<String> wordList, int maxLength){
-        Set<String> wordsWithLength = new HashSet<>();
-        for (String word : wordList) {
-            if (word.length() == maxLength) {
-                wordsWithLength.add(word);
-            }
-        }
-        return wordsWithLength;
+    public static Set<String> filterWordsByLength(Set<String> wordList, int maxLength) {
+        return wordList.stream()
+                .filter(word -> word.length() == maxLength)
+                .collect(Collectors.toSet());
     }
 
     //Remove words that are of a given length or longer since they cant combine to exact this length
-    public static Set<String> reduceWordsToMaxWordLength(Set<String> wordList, int length){
+    public static Set<String> reduceWordsToMaxWordLength(Set<String> wordList, int length) {
         wordList.removeIf(word -> word.length() >= length);
         return wordList;
     }
--- src/main/java/com/example/one16challenge/model/FileReader.java
+++ src/main/java/com/example/one16challenge/model/FileReader.java
@@ -4,32 +4,24 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.List;
+import java.nio.file.Paths;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class FileReader {
 
     //Load a set of unique words from the list
     public static Set<String> loadWordList(String filePath) throws IOException {
-
-        //Hashset to filter out duplicates and for time complexity purpose
-        Set<String> words = new HashSet<>();
-        try {
-            List<String> lines = Files.readAllLines(Path.of(filePath));
-            for (String line : lines) {
-                if (!line.isEmpty()) {
-                    words.add(line);
-                }
-            }
-
+        try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
+            return lines
+                    .filter(line -> !line.isEmpty())
+                    .collect(Collectors.toSet());
         } catch (IOException e) {
-            //TODO: add different logging
             e.printStackTrace();
+            throw e;
         }
-        return words;
     }
 
 }
